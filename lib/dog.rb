c6c60b263bfec7549709a4d7c01dbbb9da1fc802
ventura.paolo <ventura.paolo@gmail.com>
2018-09-20 12:19:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,8 +11,8 @@ end
 
 #### 2. Define `#bark` in `Dog`
 
-Add an instance method `#bark` to your `Dog` class in `lib/dog.rb` that will puts `"Woof!"`
+#Add an instance method `#bark` to your `Dog` class in `lib/dog.rb` that will puts `"Woof!"`
 
 #### 3. Define `#sit` in `Dog`
 
-Add an instance method `#sit` to your `Dog` class in `lib/dog.rb` that will puts `"The Dog is sitt
+#Add an instance method `#sit` to your `Dog` class in `lib/dog.rb` that will puts `"The Dog is sitt
